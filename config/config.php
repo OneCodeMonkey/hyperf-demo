74e9ab828742fddf0ca186e8bdef6a76c68126ed
OneCodeMonkey <1460018362@qq.com>
2019-12-27 01:06:45 +0800

    app config

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_type=1);
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Psr\Log\LogLevel;
+
+return [
+    'app_name' => env('APP_NAME', 'hyperf-demo'),
+    StdoutLoggerInterface::class => [
+        'log_level' => [
+            LogLevel::ALERT,
+            LogLevel::CRITICAL,
+            LogLevel::DEBUG,
+            LogLevel::EMERGENCY,
+            LogLevel::ERROR,
+            LogLevel::INFO,
+            LogLevel::NOTICE,
+            LogLevel::WARNING,
+        ],
+    ],
+];
